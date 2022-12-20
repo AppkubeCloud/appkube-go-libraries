3c1f098e48a71d58c3a3dd3491c6af8ed6858874
ManojSynectiks <manoj.sharma@synectiks.com>
2022-12-20 21:18:07 +0530

    appconfig

---
+++ appconfig/appconfig.go
@@ -0,0 +1,22 @@
+package appconfig
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func cmdAppConfig() *cobra.Command {
+	// o := &Options{}
+	cmd := &cobra.Command{
+		Use:   "appconfig",
+		Short: "aws appconfig",
+		Long:  "aws appconfig command",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Say helloooooooooo")
+		},
+	}
+
+	// 	// o.BaseOptions.AddBaseFlags(cmd)
+	return cmd
+}
